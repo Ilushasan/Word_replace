785891ed67cec29e9fb5b575e7228ccd63c3e46a
Ilushasan <58609885+Ilushasan@users.noreply.github.com>
2022-10-08 14:22:41 +0300

    Добавил word_replace

---
+++ word_replace.py
@@ -0,0 +1,42 @@
+#Короткий код для замены слов в фразе
+#При запуске функция просит ввести слово, которое нужно поменять
+#Потом просит слово, на которое поменять
+#Функция возвращает измененную фразу
+def replace_word():
+  # Объявление всех необходимых переменных
+  default_phrase = "Hello world, i'm Ilya" 
+  is_word = True
+  word_to_replace = ''
+  word_replacement = ''
+
+  print(f"Default phrase: {default_phrase}")
+  # Запрос слова, которое нужно заменить
+  word_to_replace = input("Enter the word to replace: ")
+  # Проверка на ввод этого слова
+  while len(word_to_replace) == 0:
+    print("Need choose the word")
+    word_to_replace = input("Enter the word to replace: ")
+
+  # Запрос слова, на которое нужно поменять
+  word_replacement = input("Enter the word replacement: ")
+  # Проверка на ввод этого слова
+  while len(word_replacement) == 0:
+    print("Need choose the word")
+    word_replacement = input("Enter the word replacement: ")
+
+  default_phrase_list = default_phrase.replace(',', '').split()
+  # Проверка на совпадение
+  for word in default_phrase_list:
+    if word_to_replace == word:
+      word_to_replace = word
+      is_word = False
+    else:
+      pass
+  # Проверка на наличие совпадений   
+  if is_word == True:
+    print("There is no match")
+  else: 
+    replace_phrase = print(default_phrase.replace(word_to_replace, word_replacement))
+    return replace_phrase
+#Запуск функции
+replace_word()
